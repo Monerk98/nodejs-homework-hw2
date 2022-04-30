d3f47e66eded11f8ee4807455601acbab15ed8da
Monerk98 <boldv98@gmail.com>
2022-04-30 12:42:49 +0300

    add
    add

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,5 +3,7 @@ const login = require('./login')
 const logout = require('./logout')
 const getCurrentUser = require('./getCurrentUser')
 const updateAvatar = require('./updateAvatar')
+const verify = require('./verify')
+const resendVerify = require('./resendVerify')
 
-module.exports = { register, login, logout, getCurrentUser, updateAvatar }
+module.exports = { register, login, logout, getCurrentUser, updateAvatar, verify, resendVerify }
